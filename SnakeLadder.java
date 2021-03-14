0bf2c024bcb9e87f71ff2b5e949225a50f3ca5c5
your name <exam@mail.com>
2021-03-15 01:15:22 +0530

    Added: UC1

---
+++ SnakeLadder.java
@@ -0,0 +1,10 @@
+package player;
+
+public class SnakeLadder {
+
+	public static void main(String[] args) {
+		int initialPos=0;
+		System.out.println("Players initial position is: "+ initialPos);
+
+	}
+}
